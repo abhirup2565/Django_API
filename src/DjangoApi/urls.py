366570de26cbd0fa149df087a2141351e456691d
abhirup2565 <abhirup2565@gmail.com>
2025-09-02 18:58:37 +0530

    rest api authentication

--- src/DjangoApi/urls.py
+++ src/DjangoApi/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from core.views import TestView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),    
+    path('api/token/',obtain_auth_token,name='obtain-token'),
     path('',TestView.as_view(),name='test')
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.views import APIView  
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializer import PostSerializer
 from .models import Post
 
 # Create your views here.
 class TestView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self,request,*args,**kwargs):
         qs = Post.objects.all()
         seralizer = PostSerializer(qs,many=True)
